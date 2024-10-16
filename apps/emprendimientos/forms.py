da0b2f78f6428c210ee996e1a2261f5759d58553
claudio-caio <161973968+claudio-caio@users.noreply.github.com>
2024-10-16 00:07:24 -0300

    nuevas actualizaciones en backend

--- apps/emprendimientos/forms.py
+++ apps/emprendimientos/forms.py
@@ -5,3 +5,14 @@ class ArticuloForm(forms.ModelForm):
     class Meta:
         model = Articulo
         fields = ['titulo',  'contenido', 'categoria', 'imagen']
+        widgets = {
+            'contenido': forms.Textarea(attrs={
+                'class': 'textarea_custom1',
+                'rows': 10,
+                'cols': 80,  
+            }),
+        }
+
+
+#Esto edite
+#         
